3449405a9af6390e3b36fbb3c51ca0a34dba8fd8
Samuel Kaessner <openglshaders@gmail.com>
2018-02-14 13:49:06 -0700

    Got files loading and sending, but table doesn't work any more.

--- client/src/Itinerary.js
+++ client/src/Itinerary.js
@@ -10,12 +10,12 @@ class Itinerary extends Component {
   createTable () {
     let distance = 0;  // need to sum this from real the trip
     let units = this.props.trip.options.distance;
-    let dests = this.props.trip.places.map((item) => <td>{item.name}</td>);
-    let dists = this.props.trip.distances.map((item) => <td>{item}</td>);
+    //let dests = this.props.trip.places.map((item) => <td>{item.name}</td>);
+    //let dists = this.props.trip.distances.map((item) => <td>{item}</td>);
 
     console.log(this.props.trip);
 
-    return {distance, units, dests, dists};
+    return {distance, units};//, dests, dists};
   }
 
   render() {
