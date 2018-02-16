fda9ddc0735dab4df34538c48a20f4e12e8d7d28
ikegentz <ikegentz@rams.colostate.edu>
2018-02-16 09:40:09 -0700

    Got wrapper SVG so that Colorado map draws inside of outer SVG, and borders draw on top of that
    Added test to make sure SVG resources are in the correct place to load

--- server/src/main/java/com/tripco/t16/planner/Trip.java
+++ server/src/main/java/com/tripco/t16/planner/Trip.java
@@ -23,7 +23,7 @@ public class Trip {
   public ArrayList<Integer> distances;
   public String map;
 
-  private final String defaultSVG = "<svg width=\"1920\" height=\"960\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\"><!-- Created with SVG-edit - http://svg-edit.googlecode.com/ --> <g> <g id=\"svg_4\"> <svg id=\"svg_1\" height=\"960\" width=\"1920\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\"> <g id=\"svg_2\"> <title>Layer 1</title> <rect fill=\"rgb(119, 204, 119)\" stroke=\"black\" x=\"0\" y=\"0\" width=\"1920\" height=\"960\" id=\"svg_3\"/> </g> </svg> </g> <g id=\"svg_9\"> <svg id=\"svg_5\" height=\"480\" width=\"960\" y=\"240\" x=\"480\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\"> <g id=\"svg_6\"> <title>Layer 2</title> <polygon points=\"0,0 960,0 960,480 0,480\" stroke-width=\"12\" stroke=\"brown\" fill=\"none\" id=\"svg_8\"/> <polyline points=\"0,0 960,480 480,0 0,480 960,0 480,480 0,0\" fill=\"none\" stroke-width=\"4\" stroke=\"blue\" id=\"svg_7\"/> </g> </svg> </g> </g> </svg>";
+  public final String defaultSVG = "<svg width=\"1920\" height=\"960\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\"><!-- Created with SVG-edit - http://svg-edit.googlecode.com/ --> <g> <g id=\"svg_4\"> <svg id=\"svg_1\" height=\"960\" width=\"1920\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\"> <g id=\"svg_2\"> <title>Layer 1</title> <rect fill=\"rgb(119, 204, 119)\" stroke=\"black\" x=\"0\" y=\"0\" width=\"1920\" height=\"960\" id=\"svg_3\"/> </g> </svg> </g> <g id=\"svg_9\"> <svg id=\"svg_5\" height=\"480\" width=\"960\" y=\"240\" x=\"480\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\"> <g id=\"svg_6\"> <title>Layer 2</title> <polygon points=\"0,0 960,0 960,480 0,480\" stroke-width=\"12\" stroke=\"brown\" fill=\"none\" id=\"svg_8\"/> <polyline points=\"0,0 960,480 480,0 0,480 960,0 480,480 0,0\" fill=\"none\" stroke-width=\"4\" stroke=\"blue\" id=\"svg_7\"/> </g> </svg> </g> </g> </svg>";
 
   /** The top level method that does planning.
    * At this point it just adds the map and distances for the places in order.
@@ -36,15 +36,11 @@ public class Trip {
 
   }
 
-  /**
-   * Returns an SVG containing the background and the legs of the trip.
-   * @return
-   */
-  private String svg() {
-    InputStream in = this.getClass().getResourceAsStream("/colorado.svg");
+  public String getSVGFromFile(String filename)
+  {
+    InputStream in = this.getClass().getResourceAsStream(filename);
     BufferedReader br;
     br = new BufferedReader(new InputStreamReader(in));
-
 
     String read = "", cur;
     try {
@@ -56,6 +52,23 @@ public class Trip {
     }
 
     return read;
+  }
+
+  /**
+   * Returns an SVG containing the background and the legs of the trip.
+   * @return
+   */
+  private String svg() {
+  String colorado = this.getSVGFromFile("/colorado.svg");
+  String borders = this.getSVGFromFile("/borders.svg");
+
+    String finalSVG =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>" +
+            "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"1066.6073\" height=\"783.0824\">\n" +
+              colorado + borders +
+            "</svg>";
+
+    return finalSVG;
   }
 
   /**
