f665889be44997b6dabf95f568c39bc97fb4fc56
ikegentz <ikegentz@rams.colostate.edu>
2018-02-20 08:41:52 -0700

    Changed to use static vars from DistanceCalculator

--- server/src/main/java/com/tripco/t16/planner/Trip.java
+++ server/src/main/java/com/tripco/t16/planner/Trip.java
@@ -10,6 +10,8 @@ import java.io.*;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 
+import com.tripco.t16.calc.DistanceCalculator;
+
 /**
  * The Trip class supports TFFI so it can easily be converted to/from Json by Gson.
  *
@@ -24,15 +26,6 @@ public class Trip {
   public String map;
 
   public final String defaultSVG = "<svg width=\"1920\" height=\"960\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\"><!-- Created with SVG-edit - http://svg-edit.googlecode.com/ --> <g> <g id=\"svg_4\"> <svg id=\"svg_1\" height=\"960\" width=\"1920\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\"> <g id=\"svg_2\"> <title>Layer 1</title> <rect fill=\"rgb(119, 204, 119)\" stroke=\"black\" x=\"0\" y=\"0\" width=\"1920\" height=\"960\" id=\"svg_3\"/> </g> </svg> </g> <g id=\"svg_9\"> <svg id=\"svg_5\" height=\"480\" width=\"960\" y=\"240\" x=\"480\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\"> <g id=\"svg_6\"> <title>Layer 2</title> <polygon points=\"0,0 960,0 960,480 0,480\" stroke-width=\"12\" stroke=\"brown\" fill=\"none\" id=\"svg_8\"/> <polyline points=\"0,0 960,480 480,0 0,480 960,0 480,480 0,0\" fill=\"none\" stroke-width=\"4\" stroke=\"blue\" id=\"svg_7\"/> </g> </svg> </g> </g> </svg>";
-
-  private final double maxLat = 37; // note these are inverted since SVG goes top to bottom
-  private final double minLat = 41; // we want out normalized plane to go top to bottom
-  private final double maxLong = -102.05;
-  private final double minLong = -109.05;
-
-  private final double latDiff = this.maxLat - this.minLat;
-  private final double longDiff = this.maxLong - this.minLong;
-
 
   /** The top level method that does planning.
    * At this point it just adds the map and distances for the places in order.
@@ -138,7 +131,8 @@ public class Trip {
    */
   public double normalizeLat(double lat)
   {
-    return (lat - this.minLat) / (this.latDiff);
+    // Note these are inverted so that we can get SVG coords going top-down instead of bottom-up
+    return (lat - DistanceCalculator.COLORADO_TOP) / (DistanceCalculator.COLORADO_BOTTOM - DistanceCalculator.COLORADO_TOP);
   }
 
   /**
@@ -148,7 +142,7 @@ public class Trip {
    */
   public double normalizeLong(double lon)
   {
-    return (lon - this.minLong) / (this.longDiff);
+    return (lon - DistanceCalculator.COLORADO_LEFT) / (DistanceCalculator.COLORADO_RIGHT - DistanceCalculator.COLORADO_LEFT);
   }
 
   /**
