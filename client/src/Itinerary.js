4faff74ba372af62d992c4a85823600383ef28a5
Samuel Kaessner <sam.kaessner@me.com>
2018-03-05 01:10:34 -0700

    Insommnia, so started on itinerary.

--- client/src/Itinerary.js
+++ client/src/Itinerary.js
@@ -20,43 +20,76 @@ class Itinerary extends Component {
     let distance = this.calcDistance();  // need to sum this from real the trip
     let units = this.props.trip.options.distance;
 
-    let dests = this.props.trip.places.map((item, index) => <td key={index}>{item.name}</td>);
-    let dists = this.props.trip.distances.map((item) => <td>{item}</td>);
 
-    if (dists.length > 0) {
-        dists.unshift(<td key={-1}> {0} </td>)
-    }
+    let dests = this.props.trip.places.map((item, index) => <td key={index}> {item.name} </td>);
+    let dists = this.props.trip.distances.map((item, index) => <td key={index}> {item}</td>);
 
     if (dests.length > 0) {
-        dests.push(<td key={-1}> {dests[0].props.children} </td>)
+        dests.push(<td key={-1}> {dests[0].props.children} </td>);
+    }
+
+    let tableData = [];
+    for (let i = 0; i < dests.length - 1; i++) {
+      tableData.push(this.renderRow(i, dests[i], dests[i + 1], dists[i]));
     }
 
 
     console.log(this.props.trip);
 
-    return {distance, units, dests, dists};
+    return {distance, units, tableData};
   }
+
+  _startDrag() {
+    console.log("Starting drag");
+  }
+
+  _endDrag() {
+    console.log("Done");
+  }
+
+  _updateDrag() {
+    console.log("Updating");
+  }
+
+  _leaveDrag() {
+    console.log("Leaving");
+  }
+
+  renderRow(key, source, destination, distance, selected) {
+    let dragHandle = (
+        <span >
+          <button className="pull-right btn btn-default"
+                  onMouseDown={this._startDrag.bind(this)}
+                  onMouseMove={this._updateDrag.bind(this)}
+                  onMouseUp={this._endDrag.bind(this)}
+                  onMouseLeave={this._leaveDrag.bind(this)}>ClickNDrag</button>
+        </span>
+    );
+
+    let opacity = selected ? 0.3 : 1.0;
+
+    return (<tr key={key} style={{opacity: opacity}}>{source}{destination}{distance}<td>{dragHandle}</td></tr>);
+  }
+
 
   render() {
     let table = this.createTable();
 
     return(
         <div id="itinerary">
-          <h4>Round trip distance of {table.distance} {table.units}. </h4>
           <table className="table table-responsive table-bordered">
             <thead>
             <tr className="table-info">
+              <th className="align-middle" style={{color: "#FFF", backgroundColor: "#3E4551"}}>Start</th>
               <th className="align-middle" style={{color: "#FFF", backgroundColor: "#3E4551"}}>Destination</th>
-              {table.dests}
+              <th className="align-middle" style={{color: "#FFF", backgroundColor: "#3E4551"}}>{table.units}</th>
             </tr>
             </thead>
             <tbody>
-            <tr>
-              <th className="table-info align-middle" style={{color: "#FFF", backgroundColor: "#3E4551"}}>{table.units}</th>
-              {table.dists}
-            </tr>
+              {table.tableData}
             </tbody>
           </table>
+          <h4>Total distance of {table.distance} {table.units}. </h4>
         </div>
     )
   }
