719cc7fecdc37653ae25991eb30ac769439521a1
Justin Going <jgoing@rams.colostate.edu>
2018-04-09 20:23:41 -0600

    Query now sends back SQL error. Need to find how to alert client side.

--- server/src/main/java/com/tripco/t16/query/DBSearcher.java
+++ server/src/main/java/com/tripco/t16/query/DBSearcher.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.tripco.t16.server.HTTP;
+import com.tripco.t16.tffi.Error;
 import spark.Request;
 
 /**
@@ -17,7 +18,7 @@ import spark.Request;
  */
 public class DBSearcher {
     private Query query;
-
+    private Error err;
     /**
      * Construct a Query object to handle TFFI transfer.
      * @param request The HTTP request specifying what we should do
@@ -35,10 +36,7 @@ public class DBSearcher {
         query = gson.fromJson(requestBody, Query.class);
 
         // do the query
-        new Find().performQuery(query, false);
-
-        // log something.
-        //System.out.println(find.some_info_here());
+        err = new Find().performQuery(query, false);
     }
 
     /**
@@ -47,6 +45,12 @@ public class DBSearcher {
      */
     public String getFind() {
         Gson gson = new Gson();
-        return gson.toJson(query);
+        if(this.err.code == "400"){
+            System.out.println("printing error");
+            return gson.toJson(err);
+        }else{
+            System.out.println("printing query");
+            return gson.toJson(query);
+        }
     }
 }
