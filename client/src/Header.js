c02bf4d3819fa5b08ee90e8e0027524be1ff6b9b
pbarstad <pbarstad@rams.colostate.edu>
2018-04-16 14:25:10 -0600

    Fixed footer syntax, just need plan to work.

--- client/src/Header.js
+++ client/src/Header.js
@@ -22,11 +22,3 @@ class Header extends Component{
 
 export default Header;
 
-/*<div id="header" className="jumbotron" style={{backgroundColor: "#1E4D28"}}>
-            <div className="col-8 col-sm-7 col-md-6 col-lg-5 col-xl-4 align-self-left">
-                <img className="img-fluid" src="http://www.cs.colostate.edu/~davematt/logos/CS_unit_identifiers/CompSci-NS-CSU-1-Hrev.png"/>
-            </div>
-            <div className="col-12 col-sm-12 col-md-6  col-lg-7 col-xl-8" >
-                <br/>
-            </div>
-        </div> */
