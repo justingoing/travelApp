e6d018b86cf910135f76e23ca424372a931c32d0
ikegentz <ikegentz@rams.colostate.edu>
2018-04-18 08:42:11 -0600

    Got buttons in correct spot. Make start of trip works except for title

--- client/src/DestCard.js
+++ client/src/DestCard.js
@@ -14,9 +14,9 @@ class DestCard extends Component {
     };
   }
 
-  static prettyPrintAirportType(airType){
+  static prettyPrintAirportType(airType) {
     airType = airType.replace(/_/g, ' ');
-    return airType.replace(/\w\S*/g, function(txt){
+    return airType.replace(/\w\S*/g, function (txt) {
       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
   }
@@ -28,44 +28,50 @@ class DestCard extends Component {
 
   render() {
     let currState = this.props;
-    let contents = function() {
+    let contents = function () {
       if (currState.destination.extraAttrs) {
         return (<p className="card-text" style={{fontSize: '75%'}}>
           <br/>
-          <b>Type:</b> {DestCard.prettyPrintAirportType(currState.destination.extraAttrs.type)}<hr/>
-          <b>City:</b> {currState.destination.extraAttrs.municipality}<hr/>
-          <b>Region:</b> {currState.destination.extraAttrs.iso_region}<hr/>
+          <b>Type:</b> {DestCard.prettyPrintAirportType(
+            currState.destination.extraAttrs.type)}
+          <hr/>
+          <b>City:</b> {currState.destination.extraAttrs.municipality}
+          <hr/>
+          <b>Region:</b> {currState.destination.extraAttrs.iso_region}
+          <hr/>
           <b>Country:</b> {currState.destination.extraAttrs.iso_country}
         </p>);
       }
     };
 
     return (
-          <div className="card" style={{width: '16rem'}}>
-            <div className="card-body">
-              <h6 className="card-title">
-                <div className="row">
-                  <div className="col-10">
-                  <span style={{color: "black"}}>{this.state.destination.name}</span>
-                  </div>
-                  <div className="col-2">
-                    <div className="row">
-                      <button type="button" className="btn btn-xs btn-primary" onClick={this.makeStart}>
-                        <FaMapMarker />
-                      </button>
-                    </div>
-                    <br/>
-                    <div className="row">
-                      <button type="button" className="btn btn-xs btn-danger">
-                        <FaMinus />
-                      </button>
-                    </div>
-                  </div>
+        <div className="card" style={{width: '16rem'}}>
+          <div className="card-body">
+            <h6 className="card-title">
+              <div className="row">
+                <div className="btn-group btn-group-sm" role="group"
+                     aria-label="What do you want do do with this destination?">
+                  <button className="btn btn-primary"
+                          onClick={this.makeStart}
+                          data-toggle="tooltip" data-placement="left"
+                          title="Make this the starting destination">
+                    <FaMapMarker/>
+                  </button>
+                  <button className="btn btn-danger"
+                          data-toggle="tooltip" data-placement="right"
+                          title="Remove this destination from the trip">
+                    <FaMinus/>
+                  </button>
                 </div>
-              </h6>
-              {contents()}
-            </div>
+              </div>
+              <br/>
+              <div className="row"
+                   style={{color: "black"}}>{this.state.destination.name}</div>
+
+            </h6>
+            {contents()}
           </div>
+        </div>
     )
   }
 }
