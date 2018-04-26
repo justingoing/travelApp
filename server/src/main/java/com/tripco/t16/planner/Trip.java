e3f2374d19eae4537c8dba7fa7f23e38148dec3e
Samuel Kaessner <sam.kaessner@me.com>
2018-04-26 12:01:16 -0600

    Added some misc fixes for trip.java

--- server/src/main/java/com/tripco/t16/planner/Trip.java
+++ server/src/main/java/com/tripco/t16/planner/Trip.java
@@ -21,18 +21,18 @@ public class Trip {
   public String map;
   public Integer version;
 
-  public ArrayList<Coords> coords;
+  private ArrayList<Coords> coords;
 
-  public static final int SVG_WIDTH = 1920;
-  public static final int SVG_HEIGHT = 960;
-  public static final int SVG_MAPPED_X = 1024;
-  public static final int SVG_MAPPED_Y = 512;
+  private static final int SVG_WIDTH = 1920;
+  private static final int SVG_HEIGHT = 960;
+  private static final int SVG_MAPPED_X = 1024;
+  private static final int SVG_MAPPED_Y = 512;
   public final String defaultSVG =
       "<svg width=\"" + Integer.toString(Trip.SVG_WIDTH) + "\" height=\"" + Integer
           .toString(Trip.SVG_HEIGHT)
           + "\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\"><!-- Created with SVG-edit - http://svg-edit.googlecode.com/ --> <g> <g id=\"svg_4\"> <svg id=\"svg_1\" height=\"960\" width=\"1920\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\"> <g id=\"svg_2\"> <title>Layer 1</title> <rect fill=\"rgb(119, 204, 119)\" stroke=\"black\" x=\"0\" y=\"0\" width=\"1920\" height=\"960\" id=\"svg_3\"/> </g> </svg> </g> <g id=\"svg_9\"> <svg id=\"svg_5\" height=\"480\" width=\"960\" y=\"240\" x=\"480\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\"> <g id=\"svg_6\"> <title>Layer 2</title> <polygon points=\"0,0 960,0 960,480 0,480\" stroke-width=\"12\" stroke=\"brown\" fill=\"none\" id=\"svg_8\"/> <polyline points=\"0,0 960,480 480,0 0,480 960,0 480,480 0,0\" fill=\"none\" stroke-width=\"4\" stroke=\"blue\" id=\"svg_7\"/> </g> </svg> </g> </g> </svg>";
 
-  public static final String DEST_RADIUS = "10";
+  private static final String DEST_RADIUS = "10";
 
   /**
    * The top level method that does planning. At this point it just adds the map and distances for
@@ -45,7 +45,6 @@ public class Trip {
         if (!this.validateLatLong(this.places.get(i).latitude) ||
             !this.validateLatLong(this.places.get(i).longitude)) {
           this.places.remove(i);
-        //}else{
           err.code = "500";
           err.message = "Server failed to validate LatLong";
           err.debug = "Trip.java-plan-validateLatLong.";
@@ -56,7 +55,7 @@ public class Trip {
       }
     }
 
-    if (options != null && options.getOptimizationLevel() >= .7) {
+    if (options != null && options.getOptimizationLevel() >= .75) {
       this.places = Optimization.optimize(places, options.getUnitRadius(), 3);
     } else if (options != null && options.getOptimizationLevel() >= .5) {
       this.places = Optimization.optimize(places, options.getUnitRadius(), 2);
@@ -100,7 +99,7 @@ public class Trip {
    *
    * @return A String SVG of trip 'legs' to be sent to the server
    */
-  public String getLegsAsSVG() {
+  private String getLegsAsSVG() {
     StringBuilder svg = new StringBuilder(
         "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"1066.6073\" height=\"783.0824\">");
 
@@ -189,21 +188,15 @@ public class Trip {
    * @param lon Longitude
    * @return Mapped coordinates as a pair
    */
-  public Coords getMappedCoords(double lat, double lon) {
-    double scaleX = Trip.SVG_MAPPED_X;
-    double scaleY = Trip.SVG_MAPPED_Y;
-    double transX = 0;
-    double transY = 0;
-
+  private Coords getMappedCoords(double lat, double lon) {
     double normalLat = this.normalizeLat(lat);
     double normalLon = this.normalizeLong(lon);
 
     // swapping lat/long since lat==y and lon==x, so we go from y/x to x/y
-    double finalX = scaleX * normalLon + transX;
-    double finalY = scaleY * normalLat + transY;
+    double finalX = SVG_MAPPED_X * normalLon;
+    double finalY = SVG_MAPPED_Y * normalLat;
 
-    Coords ret = new Coords(finalX, finalY);
-    return ret;
+    return new Coords(finalX, finalY);
   }
 
   /**
@@ -214,9 +207,6 @@ public class Trip {
    * @return Normalized latitude
    */
   public double normalizeLat(double lat) {
-    // Note these are inverted so that we can get SVG coords going top-down instead of bottom-up
-    /*return (lat - DistanceCalculator.COLORADO_TOP) / (DistanceCalculator.COLORADO_BOTTOM
-     - DistanceCalculator.COLORADO_TOP);*/
     return (lat - DistanceCalculator.WORLD_TOP) / (DistanceCalculator.WORLD_BOTTOM
         - DistanceCalculator.WORLD_TOP);
   }
@@ -228,8 +218,6 @@ public class Trip {
    * @return Normalized longitude
    */
   public double normalizeLong(double lon) {
-    /*return (lon - DistanceCalculator.COLORADO_LEFT) / (DistanceCalculator.COLORADO_RIGHT
-    - DistanceCalculator.COLORADO_LEFT); */
     return (lon - DistanceCalculator.WORLD_LEFT) / (DistanceCalculator.WORLD_RIGHT
         - DistanceCalculator.WORLD_LEFT);
   }
@@ -238,17 +226,13 @@ public class Trip {
    * Returns an SVG containing the background and the legs of the trip.
    */
   private String svg() {
-    String colorado = this.getSVGFromFile("/colorado.svg");
     String world = this.getSVGFromFile("/World4.svg");
 
-    String finalSVG =
-        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>"
+    return  "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>"
             + "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"1024\" height=\"512\">\n"
             + world
             + this.getLegsAsSVG()
             + "</svg>";
-
-    return finalSVG;
   }
 
   /**
@@ -256,9 +240,8 @@ public class Trip {
    * make a round trip.
    */
   private ArrayList<Integer> legDistances() {
-
     //Create empty list of distances and get an arraylist of coords.
-    ArrayList<Integer> dist = new ArrayList<Integer>();
+    ArrayList<Integer> dist = new ArrayList<>();
 
     //And then calculate the distances.
     for (int i = 1; i <= coords.size(); i++) {
@@ -299,14 +282,13 @@ public class Trip {
     double x;
     double y;
 
-    public Coords(double x, double y) {
+    private Coords(double x, double y) {
       this.x = x;
       this.y = y;
     }
 
     public String toString() {
-      String ret = "(" + this.x + ", " + this.y + ")";
-      return ret;
+      return "(" + this.x + ", " + this.y + ")";
     }
   }
 
@@ -317,25 +299,22 @@ public class Trip {
    * @return boolean true = valid
    */
   public boolean validateLatLong(String latIN) {
-    //System.out.println("Latitude: " + latIN);
     if (latIN.matches("\\s*\\d+[°|º]\\s*\\d+['|′]\\s*\\d+\\.?\\d*[\"|″]?\\s*[N|S|E|W]\\s*")) //DMS
     {
-      return true; //System.out.println("Matches #1");
+      return true;
     } else if (latIN
         .matches("\\s*\\d+[°|º]\\s*\\d+\\.?\\d*['|′]\\s*[N|S|E|W]\\s*")) //degrees decimal minutes
     {
-      return true; //System.out.println("Matches #2");
+      return true;
     } else if (latIN.matches("\\s*-?\\d+\\.?\\d*[°|º]\\s*[N|S|E|W]\\s*")) //decimal degrees
     {
-      return true; //System.out.println("Matches #3");
+      return true;
     } else if (latIN.matches("\\s*-?\\d+\\.?\\d*\\s*[N|S|E|W]\\s*")) //floating point
     {
-      return true; //System.out.println("Matches #4");
+      return true;
     } else if (latIN.matches("\\s*-?\\d+\\.?\\d*\\s*")) {
-      return true; //System.out.println("Matches #5");
+      return true;
     } else {
-      System.out.println("Latitude: " + latIN);
-      System.out.println("No match!");
       return false;
       //ERROR OUT??
     }
@@ -385,10 +364,8 @@ public class Trip {
 
     if (foundSecond) {
       if (conv.charAt(minuteSymbol + 1) == ' ') {
-        //System.out.println("found space");
         seconds = Double.parseDouble(conv.substring(minuteSymbol + 2, secondSymbol));
       } else {
-        //System.out.println("no spacing");
         seconds = Double.parseDouble(conv.substring(minuteSymbol + 1, secondSymbol));
       }
       seconds /= 3600; //convert seconds to degrees
@@ -414,8 +391,6 @@ public class Trip {
     } else { //just number
       degrees = Double.parseDouble(conv);
     }
-
-    //System.out.println(degrees);
 
     return degrees * direction;
   }
